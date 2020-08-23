39de6eead3dfef8f082f3308e240c1debfda65aa
johns <johns@DESKTOP-VF7AOUB>
2020-08-23 12:52:56 -0700

    i started league invaders

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,23 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	JFrame frame;
+	public static final int WIDTH=500;
+	public static final int HEIGHT = 800;
+	public static void main(String[] args) {
+	LeagueInvaders=new LeagueInvaders();	
+	
 }
+	 LeagueInvaders() {
+		 frame = new JFrame();
+		 frame.setVisible(true);
+		 
+	 }
+		
+	 void setup() {
+		 frame.setSize(WIDTH, HEIGHT);
+		 
+		 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	 }
+	 
+	}
